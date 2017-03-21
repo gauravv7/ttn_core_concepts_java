410f1d98ce8d7504448256a3b19807e560a40995
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-03-21 12:59:28 +0530

    answer of question 2

---
+++ 3_gradle/src/main/java2/AppOther.java
@@ -0,0 +1,12 @@
+/**
+ * Created by gaurav on 21/3/17.
+ */
+public class AppOther {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new AppOther().getGreeting());
+    }
+}
