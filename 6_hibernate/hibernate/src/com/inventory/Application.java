1640cf77d1663c5aa78925558faa801c5ec323f0
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-08 01:01:46 +0530

    hibernate basic structure

--- 6_hibernate/hibernate/src/com/inventory/Application.java
+++ 6_hibernate/hibernate/src/com/inventory/Application.java
@@ -1,7 +1,12 @@
 package com.inventory;
 
+import org.hibernate.Session;
+
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+
+		Session session = HibernateUtilities.getInstance().openSession();
+        session.close();
 	}
 }
---
+++ 6_hibernate/hibernate/src/com/inventory/HibernateUtilities.java
@@ -0,0 +1,33 @@
+package com.inventory;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * Created by gauravv7 on 8/4/17.
+ */
+public class HibernateUtilities {
+
+    public static SessionFactory sessionFactory;
+
+    static {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure() // configures settings from hibernate.cfg.xml
+                .build();
+        try {
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        }
+        catch (Exception e) {
+            // The registry would be destroyed by the SessionFactory, but we had trouble building the SessionFactory
+            // so destroy it manually.
+            StandardServiceRegistryBuilder.destroy( registry );
+        }
+    }
+
+    public static SessionFactory getInstance(){
+        return sessionFactory;
+    }
+}
