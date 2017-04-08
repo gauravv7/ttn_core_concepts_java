ce1786e5727e1de898469d3bf97ea557b628545d
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-08 17:04:23 +0530

    answer of question 4

--- 6_hibernate/hibernate/src/com/inventory/Application.java
+++ 6_hibernate/hibernate/src/com/inventory/Application.java
@@ -16,13 +16,41 @@ public class Application {
 		
 		session.beginTransaction();
 		
-		Author author = new Author();
+		Author author1 = new Author();
 		
-		author.setFirstName("abc");
-		author.setLastName("xyz");
-		author.setAge(20);
-		author.setDob(new SimpleDateFormat("dd/mm/yyyy").parse("04/03/1992"));
-		session.save(author);
+		author1.setFirstName("abc");
+		author1.setLastName("xyz");
+		author1.setAge(20);
+		author1.setDob(new SimpleDateFormat("dd/mm/yyyy").parse("04/03/1992"));
+		session.save(author1);
+		author1.setAge(author1.getAge()+1);
+		
+		Author author2 = new Author();
+		
+		author2.setFirstName("def");
+		author2.setLastName("xyz");
+		author2.setAge(20);
+		author2.setDob(new SimpleDateFormat("dd/mm/yyyy").parse("05/03/1992"));
+		session.save(author2);
+		author2.setAge(author2.getAge()+2);
+		
+		Author author3 = new Author();
+		
+		author3.setFirstName("ghi");
+		author3.setLastName("xyz");
+		author3.setAge(20);
+		author3.setDob(new SimpleDateFormat("dd/mm/yyyy").parse("06/03/1992"));
+		session.save(author3);
+		author3.setAge(author3.getAge()+3);
+
+		Author author4 = new Author();
+		
+		author4.setFirstName("jkl");
+		author4.setLastName("xyz");
+		author4.setAge(20);
+		author4.setDob(new SimpleDateFormat("dd/mm/yyyy").parse("07/03/1992"));
+		session.save(author4);
+		author4.setAge(author4.getAge()+4);
 
 		session.getTransaction().commit();
 		
