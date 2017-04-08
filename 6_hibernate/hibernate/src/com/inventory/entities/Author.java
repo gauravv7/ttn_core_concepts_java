0158e66a57d883001fd3b450e66b9bc9976ed464
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-08 17:54:41 +0530

    answer of question 6 to 9

--- 6_hibernate/hibernate/src/com/inventory/entities/Author.java
+++ 6_hibernate/hibernate/src/com/inventory/entities/Author.java
@@ -2,11 +2,35 @@ package com.inventory.entities;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="author")
 public class Author {
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(name="fName")
 	private String firstName;
+	
+	@Transient
 	private String lastName;
+	
+	@Column(name="age")
 	private int age;
+	
+	@Column(name="date_of_birth")
+	@Temporal(TemporalType.DATE)
 	private Date dob;
 	
 	public int getId() {
